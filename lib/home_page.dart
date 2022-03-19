f1973a00d02e58dd7daa98b72c98e9474705599c
davronbek199 <zvezdadavid1@gmail.com>
2022-03-19 21:32:57 +0500

    changes

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,48 +1,40 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:lesson_three/details_page.dart';
 import 'package:lesson_three/words.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int selectedPage = 0;
 
   get list => Words.list;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Words"),
-      ),
-      body: ListView(
-        children: [
-          myWord(list[0], context),
-          myWord(list[1], context),
-          myWord(list[2], context),
-          myWord(list[3], context),
-          myWord(list[4], context),
-          myWord(list[5], context),
-          myWord(list[6], context),
-          myWord(list[7], context),
-          myWord(list[8], context),
-          myWord(list[9], context),
-          myWord(list[10], context),
-          myWord(list[11], context),
-        ],
-      ),
-    );
+    MediaQueryData mediaQueryData = MediaQuery.of(context);
+    var orientation = mediaQueryData.orientation;
+
+    return orientation == Orientation.portrait
+        ? forPortrait(context)
+        : forLandscape();
   }
 
   Widget myWord(Words words, context) {
     return InkWell(
       onTap: () {
         Navigator.push(
-            context,
-            MaterialPageRoute(
-                builder: (context) => DetailsPage(
-                      words: words,
-                    )));
+          context,
+          MaterialPageRoute(
+            builder: (context) => DetailsPage(
+              words: words,
+            ),
+          ),
+        );
       },
       child: Card(
         margin: EdgeInsets.all(1),
@@ -69,6 +61,64 @@ class HomePage extends StatelessWidget {
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget forPortrait(context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Words"),
+      ),
+      body: ListView(
+        children: [
+          myWord(list[0], context),
+          myWord(list[1], context),
+          myWord(list[2], context),
+          myWord(list[3], context),
+          myWord(list[4], context),
+          myWord(list[5], context),
+          myWord(list[6], context),
+          myWord(list[7], context),
+          myWord(list[8], context),
+          myWord(list[9], context),
+          myWord(list[10], context),
+          myWord(list[11], context),
+        ],
+      ),
+    );
+  }
+
+  Widget forLandscape() {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Words"),
+      ),
+      body: Row(
+        children: [
+          Container(
+            width: 240,
+            child: ListView(
+              children: [
+                myWord(list[0], context),
+                myWord(list[1], context),
+                myWord(list[2], context),
+                myWord(list[3], context),
+                myWord(list[4], context),
+                myWord(list[5], context),
+                myWord(list[6], context),
+                myWord(list[7], context),
+                myWord(list[8], context),
+                myWord(list[9], context),
+                myWord(list[10], context),
+                myWord(list[11], context),
+
+
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
